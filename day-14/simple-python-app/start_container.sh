79e51b5764b2e5dfed45c669f261494d5c7cb0eb
chandrahas20 <89960588+chandrahas20@users.noreply.github.com>
2023-12-26 10:08:34 +0530

    Update start_container.sh

--- day-14/simple-python-app/start_container.sh
+++ day-14/simple-python-app/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-echo
+docker pull chandra1243/simple-python-app
 
 # Run the Docker image as a container
-echo
+docker run -d -p 5000:5000 chandra1243/simple-python-app
